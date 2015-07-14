54554d4235e12cfb1ce76b3fff43a9eb03b84bd3
James King <ripixel@gmail.com>
2015-07-14 21:04:06 +0100

    Sorting out styling/images placement :art:

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Tramlines Planner</title>
-    <link href="pure-min.css" rel="stylesheet" type="text/css" />
-    <link href="grids-responsive-min.css" rel="stylesheet" type="text/css" />
-    <link href="tramlinesplanner.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/pure-min.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/grids-responsive-min.css" rel="stylesheet" type="text/css" />
+    <link href="public/css/tramlinesplanner.css" rel="stylesheet" type="text/css" />
     <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 </head>
