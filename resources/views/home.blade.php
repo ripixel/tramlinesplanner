a4327b743884fe33991a02cb769c4f2bf611569b
James King <ripixel@gmail.com>
2015-07-16 19:00:06 +0100

    Small changes :fish:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,12 @@
 
 <div class="hero">
     <div class="container hero-center">
-        <h1 class="title hide-mobile"><img src="/tramlinesplanner/public/img/tramlines-text.png" /><br/><span style="color: #ed1c24; font-size: 1.4em;">Tramlines Festival Schedule Planner</span></h1>
+        <h1 class="title hide-mobile"><img src="/img/tramlines-text.png" /><br/><span style="color: #ed1c24; font-size: 1.4em;">Tramlines Festival Schedule Planner</span></h1>
         <p class="restrict-width">An easy to use planner for Tramlines. Tell us who you want to see, and we'll figure out where you need to be, and if there's any clashes. Alternatively, see the whole schedule and pick what you want to see!</p>
         <p class="restrict-width">See your schedule, share it with friends, and even enter your friends' email address to see which gigs you're both going to!</p>
+
+        <h2>Signing up takes just <span class="tram-red">30 seconds</span></h2>
+        <p>It's literally so we know who's schedule is who's - we'll never email you! Just click on one of the options below to get started:</p>
 
         <div class="pure-g select-box-grid">
             <a href="{{ action('ByArtistController@show') }}" class="pure-u-md-1-3 pure-u-1 select-box-a">
--- resources/views/shareschedule.blade.php
+++ resources/views/shareschedule.blade.php
@@ -3,11 +3,31 @@
 <div class="content">
     <div class="container">
 
-        <h2>Shared <span class="tram-red">Tramlines</span> Schedule</h2>
-        <p>Below are all the events that {{ $user->name }} has said they're going to at Tramlines 2015. Create your own schedule using the links above, or use the "Compare Schedules" link if you already have one, and we'll tell you if you're both going to the same things!</p>
+        <h2>{{ $user->name }} has organised their <span class="tram-red">Tramlines</span> Schedule</h2>
 
-        <h3>{{ $user->name }} is going to <span class="noOfEventsSelected tram-red">{{ $timings->count() }}</span> events!</h3>
+        <h3>They're is going to <span class="noOfEventsSelected tram-red">{{ $timings->count() }}</span> events!</h3>
         @include('partials.schedule')
+
+        <h2>Want to figure out your own schedule?</h2>
+        <div class="pure-g center-me">
+            <a href="{{ action('ByArtistController@show') }}" class="pure-u-md-1-2 pure-u-1 select-box-a">
+                <div class="pure-u-1">
+                    <div style="min-height: 0;" class="select-box">
+                        <h2>Plan By Artist</h2>
+                        <p>Plan your weekend by selecting your favourite artists - we'll figure out your schedule</p>
+                    </div>
+                </div>
+            </a>
+
+            <a  href="{{ action('ByTimeController@show') }}" class="pure-u-md-1-2 pure-u-1 select-box-a">
+                <div class="pure-u-1">
+                    <div style="min-height: 0;" class="select-box">
+                        <h2>Plan By Time</h2>
+                        <p>Plan your weekend by seeing the times and selecting who you want to see</p>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 
